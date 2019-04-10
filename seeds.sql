49e92e9088aca758d7f95d4033e3b6fee5f8e1b2
Wesley Berry <wesleyberry52@gmail.com>
2019-04-10 18:22:42 -0500

    added seeds

--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,5 @@
 INSERT INTO bands (username, password) VALUES ("bandUsername", "bandPassword");
 
+INSERT INTO bands (username, password) VALUES ("bandUsername2", "bandPassword2");
+
 INSERT INTO managers (username, password) VALUES ("managerUsername", "managerPassword");
